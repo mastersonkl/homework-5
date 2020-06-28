dcae699c5851fcc03f932ff446fca0f4283455ec
Kendall Masterson <masterson.kendall@gmail.com>
2020-06-28 14:37:39 -0400

    Updated it to pull all time blocks from local storage

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ $(document).ready(function () {
 
         console.log(timeBlock)
 
-        localStorage.setItem(innerText, timeBlock);
+        localStorage.setItem(timeBlock, innerText);
 
 
     });
@@ -56,6 +56,14 @@ $(document).ready(function () {
 colorUpdate();
 
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
 });
 
